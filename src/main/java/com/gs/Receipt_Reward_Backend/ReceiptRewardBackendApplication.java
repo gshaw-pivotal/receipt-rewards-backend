0319274021c7d96faa6a702e23803736609f0703
Gavin Shaw <gavshaw+github@gmail.com>
2024-11-25 01:24:00 -0500

    Add skeleton Java Spring Boot application

---
+++ src/main/java/com/gs/Receipt_Reward_Backend/ReceiptRewardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gs.Receipt_Reward_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptRewardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptRewardBackendApplication.class, args);
+	}
+
+}
