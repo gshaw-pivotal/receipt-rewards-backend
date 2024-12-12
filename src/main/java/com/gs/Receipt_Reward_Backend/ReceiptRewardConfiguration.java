7858954945147e85dbbce22d87254f6a55f88344
Gavin <gavin@Gavins-MacBook-Pro.local>
2024-12-13 08:24:30 +1000

    Add an interface for the reward point calculation
    - The existing implementation is now 'DefaultPointsCalculator' and
      implements the interface. This will allow differing implementations of
      a point calcualtor to be implemented and used by swapping them out in
      the configuration.

--- src/main/java/com/gs/Receipt_Reward_Backend/ReceiptRewardConfiguration.java
+++ src/main/java/com/gs/Receipt_Reward_Backend/ReceiptRewardConfiguration.java
@@ -3,6 +3,7 @@ package com.gs.Receipt_Reward_Backend;
 import com.gs.Receipt_Reward_Backend.database.InMemoryReceiptRewardRepository;
 import com.gs.Receipt_Reward_Backend.database.ReceiptRewardRepository;
 import com.gs.Receipt_Reward_Backend.service.ReceiptRewardService;
+import com.gs.Receipt_Reward_Backend.service.DefaultPointsCalculator;
 import com.gs.Receipt_Reward_Backend.service.RewardPointsCalculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,7 @@ public class ReceiptRewardConfiguration {
 
     @Bean
     public RewardPointsCalculator rewardPointsCalculator() {
-        return new RewardPointsCalculator();
+        return new DefaultPointsCalculator();
     }
 
     @Bean
